40bcffae43b4b67b1496292959a6d5c124217fa3
porterrigby <prigby.a@gmail.com>
2025-01-06 19:27:53 -0700

    Fixes turn_in_place() script function. Removes redundant methods from coppelia_cozmo.

--- retico_coppelia/coppelia_cozmo.py
+++ retico_coppelia/coppelia_cozmo.py
@@ -52,10 +52,6 @@ class Cozmo:
                 while any(thread.is_alive() for thread in self.threads.values()): continue
             self._unlock()
 
-
-    FRONT_WHEEL_RADIUS = 0.165  # 0.165
-    BACK_WHEEL_RADIUS = 0.125  # 0.125
-    WHEELBASE_RADIUS = 0.247  # 0.243
     MAX_LIFT_POS = MAX_HEAD_POS = 0.785
     MIN_LIFT_POS = MIN_HEAD_POS = 0
 
@@ -84,12 +80,6 @@ class Cozmo:
         self._joint_handles['head'] = self._sim.getObject(cozmo_path + '/camera_joint')
         self._joint_handles['lift'] = self._sim.getObject(cozmo_path + '/arm_joint')
 
-        # Initialize joint velocities
-        self._sim.setJointTargetVelocity(self._joint_handles['left_front'], 0)
-        self._sim.setJointTargetVelocity(self._joint_handles['left_back'], 0)
-        self._sim.setJointTargetVelocity(self._joint_handles['right_front'], 0)
-        self._sim.setJointTargetVelocity(self._joint_handles['right_back'], 0)
-
         if self.start_scene:
             print("Starting simulation...")
             self._sim.startSimulation()
@@ -101,73 +91,13 @@ class Cozmo:
 
     def wait_until_completed(self):
         while self._sim.callScriptFunction("is_moving", self._script_handle):
-            print("Waiting...")
             time.sleep(0.1)
-        print("Completed.")
 
     def turn_in_place(self, angle: AngleDistance, speed: Speed):
         while self._simlock: continue
         self._sim.callScriptFunction("turn_in_place", self._script_handle, angle.to_radians(), speed.to_rads())
 
         return self
-
-
-    @staticmethod
-    def _turn_in_place(sim, joints, angle: AngleDistance, speed: Speed, radi: list):
-        if angle.magnitude == 0: return
-
-        # # Calculate velocity for front and rear wheels
-        angular_vel_front = 2 * (speed.to_rads() * radi[0]) / radi[1]
-        angular_vel_back = 2 * (speed.to_rads() * radi[0]) / radi[2]
-
-        # # Calculate time until joint has fully rotated
-        t = angle.get_wait_time(speed)
-        # print(f"Wait time: {t}")
-
-        end_pos_left = sim.getJointPosition(joints[0]) + angle.to_radians()
-        if end_pos_left > np.pi:
-            end_pos_left -= 2 * np.pi
-        elif end_pos_left < -np.pi:
-            end_pos_left += 2 * np.pi
-
-        end_pos_right = sim.getJointPosition(joints[2]) - angle.to_radians()
-        if end_pos_right > np.pi:
-            end_pos_right -= 2 * np.pi
-        elif end_pos_right < -np.pi:
-            end_pos_right += 2 * np.pi
-
-        # # Calculate velocity for front and rear wheels
-        angular_vel_front = 2 * (speed.to_rads() * radi[0]) / radi[1]
-        angular_vel_back = 2 * (speed.to_rads() * radi[0]) / radi[2]
-
-        # # Calculate time until joint has fully rotated
-        t = angle.get_wait_time(speed)
-        # print(f"Wait time: {t}")
-
-        if angle.magnitude > 0:
-            sim.setJointTargetVelocity(joints[0], -angular_vel_front)
-            sim.setJointTargetVelocity(joints[1], -angular_vel_back)
-            sim.setJointTargetVelocity(joints[2], angular_vel_front)
-            sim.setJointTargetVelocity(joints[3], angular_vel_back)
-        else:
-            sim.setJointTargetVelocity(joints[0], angular_vel_front)
-            sim.setJointTargetVelocity(joints[1], angular_vel_back)
-            sim.setJointTargetVelocity(joints[2], -angular_vel_front)
-            sim.setJointTargetVelocity(joints[3], -angular_vel_back)
-
-        # # Wait for joint to rotate
-        time.sleep(t)
-
-        sim.setJointTargetVelocity(joints[0], 0)
-        sim.setJointTargetVelocity(joints[1], 0)
-        sim.setJointTargetVelocity(joints[2], 0)
-        sim.setJointTargetVelocity(joints[3], 0)
-
-        # Correct the position
-        sim.setJointPosition(joints[0], end_pos_left)
-        sim.setJointPosition(joints[1], end_pos_left)
-        sim.setJointPosition(joints[2], end_pos_right)
-        sim.setJointPosition(joints[3], end_pos_right)
 
     def set_head_angle(self, angle: AngleDistance, speed: Speed):
         while self._simlock: continue
@@ -188,13 +118,6 @@ class Cozmo:
 
         return thread
 
-    @staticmethod
-    def _set_head_angle(sim, joint, angle: AngleDistance, speed: Speed, radius: float):
-        sim.setJointTargetPosition(joint, angle.magnitude)  # TODO utilize speed for movement
-
-        # Calculate time for joint to move and wait before terminating
-        time.sleep(angle.get_wait_time(speed, radius))
-
     def set_lift_height(self, height: float, speed: Speed):
         while self._simlock: continue
 
@@ -212,19 +135,6 @@ class Cozmo:
         thread.start()
 
         return thread
-
-    @staticmethod
-    def _set_lift_height(sim, joint, height: float, speed: Speed):
-        if height < Cozmo.MIN_LIFT_POS:
-            height = 0
-        elif height > Cozmo.MAX_LIFT_POS:
-            height = Cozmo.MAX_LIFT_POS
-
-        sim.setJointTargetPosition(joint, height)  # TODO utilize speed for movement
-
-        # Calculate time for joint to move and wait before terminating
-        angle = Radians(height)
-        time.sleep(angle.magnitude / speed.rate)
 
     def drive_straight(self, distance: AngleDistance, speed: Speed):
         while self._simlock: continue
@@ -249,29 +159,6 @@ class Cozmo:
         thread.start()
 
         return thread
-
-    @staticmethod
-    def _drive_straight(sim, joints, distance: AngleDistance, speed: Speed, radius: float):
-        if distance.magnitude > 0:
-            # trigger front wheel velocities before back wheel velocities
-            sim.setJointTargetVelocity(joints[0], speed.to_rads() / radius)
-            sim.setJointTargetVelocity(joints[1], speed.to_rads() / radius)
-            sim.setJointTargetVelocity(joints[2], speed.to_rads() / radius)
-            sim.setJointTargetVelocity(joints[3], speed.to_rads() / radius)
-        else:
-            # trigger back wheel velocities before front wheel velocities
-            sim.setJointTargetVelocity(joints[3], -speed.to_rads() / radius)
-            sim.setJointTargetVelocity(joints[2], -speed.to_rads() / radius)
-            sim.setJointTargetVelocity(joints[1], -speed.to_rads() / radius)
-            sim.setJointTargetVelocity(joints[0], -speed.to_rads() / radius)
-
-        # Calculate how long to turn joints...
-        time.sleep(distance.get_wait_time(speed))
-
-        sim.setJointTargetVelocity(joints[0], 0)
-        sim.setJointTargetVelocity(joints[1], 0)
-        sim.setJointTargetVelocity(joints[2], 0)
-        sim.setJointTargetVelocity(joints[3], 0)
 
 
 class CoppeliaCozmoRobot(retico_core.AbstractModule):
@@ -312,10 +199,10 @@ class CoppeliaCozmoRobot(retico_core.AbstractModule):
         iu = self.queue.pop(0)
         command = iu.payload
         if "turn left" in command:
-            self.robot.turn_in_place(angle=Radians(3 * np.pi), speed=RadiansPerSecond(np.pi)).wait_until_completed()
+            self.robot.turn_in_place(angle=Radians(2 * np.pi), speed=RadiansPerSecond(np.pi / 2)).wait_until_completed()
             # self.robot.turn_in_place(angle=Radians(2 * np.pi), speed=RadiansPerSecond(np.pi/2))
         if "turn right" in command:
-            self.robot.turn_in_place(angle=Radians(3 * -np.pi), speed=RadiansPerSecond(np.pi)).wait_until_completed()
+            self.robot.turn_in_place(angle=Radians(2 * -np.pi), speed=RadiansPerSecond(np.pi / 2)).wait_until_completed()
             # self.robot.turn_in_place(angle=Radians(2 * -np.pi), speed=RadiansPerSecond(np.pi/2))
         if "look up" in command:
             self.robot.set_head_angle(angle=Radians(self.max_head_angle), speed=RadiansPerSecond(1)).wait_until_completed()
