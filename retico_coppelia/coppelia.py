d0e22d9be130c4ff85a401ce9daf6345e514e315
porterrigby <prigby.a@gmail.com>
2025-01-12 11:06:57 -0700

    Adds handling for multiple IU types. Adds JointForceIU.

--- retico_coppelia/coppelia.py
+++ retico_coppelia/coppelia.py
@@ -2,42 +2,83 @@ from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 import retico_core
 
 
-class CoppeliaJointIU(retico_core.abstract.IncrementalUnit):
+class JointForceIU(retico_core.abstract.IncrementalUnit):
 
     @staticmethod
     def type():
-        return "CoppeliaJointIU"
+        return "JointForceIU"
 
-    def __init__(self, creator=None, iuid=0, previous_iu=None,
-                 grounded_in=None, payload=None, **kwargs):
-        super().__init__(creator=creator, iuid=iuid, previous_iu=previous_iu,
-                         grounded_in=grounded_in, payload=payload)
-        self.payload = payload
+    def __init__(self, creator=None, iuid=0, previous_iu=None, grounded_in=None, payload: dict[str: list]=None, **kwargs):
+        super().__init__(creator=creator, iuid=iuid, previous_iu=previous_iu, grounded_in=grounded_in, payload=payload)
+        if payload is not None:
+            self.payload = payload
+        else:
+            self.payload = {}
+
+    def set_single_force(self, joint_path, force):
+        self.payload[joint_path] = force
+
+    def set_multi_force(self, list_of_joint_paths, list_of_forces):
+        for path, force in list(zip(list_of_joint_paths, list_of_forces)):
+            self.payload[path] = force
 
     def __str__(self):
-        return "(CoppeliaJointIU: " + self.payload + ")"
+        return f"(JointForceIU: {self.payload.items()})"
 
 
-class CoppeliaCozmoIU(retico_core.abstract.IncrementalUnit):
+class JointVelocityIU(retico_core.abstract.IncrementalUnit):
+
     @staticmethod
     def type():
-        return "CoppeliaCozmoIU"
+        return "JointVelocityIU"
 
-    def __init__(self, creator=None, iuid=0, previous_iu=None,
-                 grounded_in=None, payload=None, **kwargs):
-        super().__init__(creator=creator, iuid=iuid, previous_iu=previous_iu,
-                         grounded_in=grounded_in, payload=payload)
-        self.payload = payload
+    def __init__(self, creator=None, iuid=0, previous_iu=None, grounded_in=None, payload: dict[str: list]=None, **kwargs):
+        super().__init__(creator=creator, iuid=iuid, previous_iu=previous_iu, grounded_in=grounded_in, payload=payload)
+        if payload is not None:
+            self.payload = payload
+        else:
+            self.payload = {}
+
+    def set_single_velocity(self, joint_path, vel):
+        self.payload[joint_path] = vel
+
+    def set_multi_velocity(self, list_of_joint_paths, list_of_velocities):
+        for path, vel in list(zip(list_of_joint_paths, list_of_velocities)):
+            self.payload[path] = vel
 
     def __str__(self):
-        return "(CoppeliaCozmoIU: " + self.payload + ")"
+        return f"(JointVelocityIU: {self.payload.items()})"
 
 
+class JointPositionIU(retico_core.abstract.IncrementalUnit):
 
-class CoppeliaSimController(retico_core.AbstractConsumingModule):
+    @staticmethod
+    def type():
+        return "JointPositionIU"
+
+    def __init__(self, creator=None, iuid=0, previous_iu=None, grounded_in=None, payload: dict[str: list]=None, **kwargs):
+        super().__init__(creator=creator, iuid=iuid, previous_iu=previous_iu, grounded_in=grounded_in, payload=payload)
+        if payload is not None:
+            self.payload = payload
+        else:
+            self.payload = {}
+
+    def set_single_position(self, joint_path, pos):
+        self.payload[joint_path] = pos
+
+    def set_multi_position(self, list_of_joint_paths, list_of_positions):
+        for path, pos in list(zip(list_of_joint_paths, list_of_positions)):
+            self.payload[path] = pos
+
+    def __str__(self):
+        return f"(JointPositionIU: {self.payload.items()})"
+
+
+class CoppeliaModule(retico_core.AbstractConsumingModule):
+
     @staticmethod
     def name():
-        return "CoppeliaSimController"
+        return "CoppeliaModule"
 
     @staticmethod
     def description():
@@ -45,7 +86,7 @@ class CoppeliaSimController(retico_core.AbstractConsumingModule):
 
     @staticmethod
     def input_ius():
-        return [CoppeliaJointIU]
+        return [JointPositionIU, JointVelocityIU, JointForceIU]
 
     @staticmethod
     def output_iu():
@@ -55,7 +96,6 @@ class CoppeliaSimController(retico_core.AbstractConsumingModule):
         super().__init__(**kwargs)
 
         self.start_scene = start_scene
-
         self.client = RemoteAPIClient()
         self.sim = self.client.require('sim')
         self.sim.loadScene(scene)
@@ -70,24 +110,18 @@ class CoppeliaSimController(retico_core.AbstractConsumingModule):
                 self.process_iu(iu)
 
     def process_iu(self, iu):
-        for joint in iu.payload:
-            handle = self.sim.getObject(joint)
-
-            if 'vel' in iu.payload[joint]:
-                vel = iu.payload[joint]['vel']
-                print(handle, vel)
+        if type(iu) is JointForceIU:
+            for path, force in iu.payload.items():
+                handle = self.sim.getObject(path)
+                self.sim.setJointTargetForce(handle, force)
+        elif type(iu) is JointVelocityIU:
+            for path, vel in iu.payload.items():
+                handle = self.sim.getObject(path)
                 self.sim.setJointTargetVelocity(handle, vel)
-
-            if 'pos' in iu.payload[joint]:
-                pos = iu.payload[joint]['pos']
-                print(handle, pos)
+        elif type(iu) is JointPositionIU:
+            for path, pos in iu.payload.items():
+                handle = self.sim.getObject(path)
                 self.sim.setJointTargetPosition(handle, pos)
-
-    def _set_joint_pos(self, handle, pos):
-        self.sim.setJointTargetPosition(handle, pos)
-
-    def _set_joint_vel(self, handle, vel):
-        self.sim.setJointTargetVelocity(handle, vel)
 
     def shutdown(self):
         if self.start_scene:
